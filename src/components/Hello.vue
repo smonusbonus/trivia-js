d7e705d83b3cff38eeb41fed097f39b48efd73b1
smonusbonus <simokrei@gmail.com>
2017-02-28 18:12:15 +0100

    Add first trivia question and some interactivity

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,7 +2,16 @@
   <div class="hello">
     <h1>{{ message }}</h1>
     <p>JS Trivia is a fun way to test you Javascript knowledge and become a better programmer!</p>
-    <button type="button">Try me!</button>
+    <button type="button" v-on:click="showQuestions">Try me!</button>
+    <div class="questions" v-if="tryMe">
+      <div class="question" v-for="question in questions">
+        <h2>{{ question.question }}</h2>
+        <div>
+          <p>Answers:</p>
+          <button type="button" v-for="answer in question.answers">{{ answer }}</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +21,15 @@ export default {
   data() {
     return {
       message: 'Welcome to JS Trivia!',
+      tryMe: false,
+      showQuestions: () => {
+        this.tryMe = true;
+      },
+      questions: [{
+        question: 'What will typeof null return?',
+        answers: ['object', 'null', 'undefined', 'number'],
+        solution: 0,
+      }],
     };
   },
 };
@@ -45,5 +63,11 @@ button {
   border: 0;
   outline: 0;
   border-radius: 0.5rem;
+  margin: 0 0.5rem;
+}
+
+button:hover {
+  opacity: 0.7;
+  cursor: pointer;
 }
 </style>
