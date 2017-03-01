2fa012e590452e67ccd441c25f5a3750a7121e7b
smonusbonus <simokrei@gmail.com>
2017-03-01 20:34:00 +0100

    Fix specs

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -6,6 +6,6 @@ describe('Hello.vue', () => {
     const Constructor = Vue.extend(Hello);
     const vm = new Constructor().$mount();
     expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App');
+      .to.equal('Welcome to JS Trivia!');
   });
 });
