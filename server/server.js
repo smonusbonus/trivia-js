b97d551764b970f80cee7d8c3d347399287570f9
smonusbonus <simokrei@gmail.com>
2017-03-11 13:05:12 +0100

    Add a simple static file server

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.listen(port);
