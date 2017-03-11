bfce88305c4cf92c8d599facf1b8c6d8954a0809
smonusbonus <simokrei@gmail.com>
2017-03-11 23:12:46 +0100

    Use VueHead to add meta tag for mobile devices

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,11 @@ export default {
   components: {
     Hello,
   },
+  head: {
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import vueResource from 'vue-resource';
+import VueHead from 'vue-head';
+import VueResource from 'vue-resource';
 import App from './App';
 
 require('font-awesome-webpack');
 
 Vue.config.productionTip = false;
-Vue.use(vueResource);
+Vue.use(VueHead);
+Vue.use(VueResource);
 
 /* eslint-disable no-new */
 new Vue({
