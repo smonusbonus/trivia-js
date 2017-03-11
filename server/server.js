ba5ea6137d2aba70412a61ee1482026d04dec5f7
smonusbonus <simokrei@gmail.com>
2017-03-11 15:38:24 +0100

    Turn questions.json into server-side module

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
+const questions = require('./questions');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+app.get('/questions', (req, res) => {
+  res.json(questions);
+});
+
 app.listen(port);
