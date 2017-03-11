23d56eb3aadc754ff8959dd9264c67650dde75f8
smonusbonus <simokrei@gmail.com>
2017-03-11 15:39:12 +0100

    Add vue-resource for handling http requests

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import vueResource from 'vue-resource';
 import App from './App';
 
 Vue.config.productionTip = false;
+Vue.use(vueResource);
 
 /* eslint-disable no-new */
 new Vue({
