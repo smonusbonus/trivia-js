1a5d62255e99af3a135730f5d41a01ac3b37c71c
smonusbonus <simokrei@gmail.com>
2017-03-12 14:49:38 +0100

    Add headline to explanation pragraph

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -19,6 +19,7 @@
       <div class="welcome-text">
         <h1>Welcome!</h1>
         <p class="lead">trivia.js is a fun way to test you Javascript knowledge and become a better programmer.</p>
+        <strong>How does it work?</strong>
         <p>We'll ask 10 random questions and you've got 10 seconds to answer each one. The quicker you answer, the more points you get. Ready?</p>
       </div>
       <div>
